3de06d03248bf919dd8173a9fd719997c309ba12
Lupetto Lavoretto <lupettolavoretto@gmail.com>
2024-12-05 14:31:09 +0100

    Code: esperimento personale per box "hai trovato un oggetto!"

---
+++ Assets/Scripts/ObjectFound.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+
+public class ObjectFound : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject infoPanel;
+    [SerializeField] private TextMeshProUGUI infoText;
+
+    public bool objectFound = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objectFound = false;
+        infoPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectFound == false)
+        {
+            return;
+        }
+        else
+        {   infoPanel.SetActive(true);
+            infoText.text = "Hai trovato un oggetto!";
+        }
+    }
+
+
+}   
