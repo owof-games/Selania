d293fa676ede2a7f4a342d20f59f09b5538e1653
LupettoLavoretto <114333442+LupettoLavoretto@users.noreply.github.com>
2024-12-28 15:07:36 +0100

    code: tentato di settare upload ma in modo incasinato

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,10 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    public void Start()
+    {
+     
+
+    }
+
     public void OnPlayButton ()
-        {
-            SceneManager.LoadScene(1);
-        }
+    {
+        SceneManager.LoadScene(1);
+    }
 
     public void OnQuitButton ()
     {
@@ -16,6 +23,7 @@ public class Menu : MonoBehaviour
 
     public void OnContinueButton ()
     {
+            //La mia idea è quella di poter chiamare l'oggetto che ha il dialogueManager e chiamare da lì la funzione LoadGame(). Ha senso?
 
     }
 
