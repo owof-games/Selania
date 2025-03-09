838e2645f807e8833e7a81646dfd89f25852c695
LupettoLavoretto <114333442+LupettoLavoretto@users.noreply.github.com>
2025-03-09 11:33:20 +0100

    code: tasto continue/new game

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,21 +1,55 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
 public class Menu : MonoBehaviour
 {
 
+    [SerializeField] Button continueButton;
 
-    public void OnPlayButton ()
+
+    void Start()
+    {
+        var fileName = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(fileName))
+        {
+
+            continueButton.interactable = true;
+        }
+
+        else
+        {
+
+            continueButton.interactable = false;
+
+        }
+
+    }
+
+    public void OnPlayButton()
     {
         SceneManager.LoadScene(2);
     }
 
-    public void OnQuitButton ()
+    public void OnQuitButton()
     {
         Application.Quit();
     }
-    
+
+
+    public void OnNewGameButton()
+    {
+        var fileName = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(fileName))
+        {
+
+            File.Delete(fileName);
+        }
+
+        SceneManager.LoadScene(2);
+    }
 
 }
