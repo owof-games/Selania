6b9b38b4d373333174d3c9e382b50da613098243
mattia <mattia.belletti@gmail.com>
2026-06-16 19:37:51 +0200

    feat: start the game from the last save state.

--- Assets/Rework/Components/MainSceneController.cs
+++ Assets/Rework/Components/MainSceneController.cs
@@ -1,3 +1,6 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Selania.Rework.Interfaces;
 using UnityEngine;
 using VContainer;
@@ -10,14 +13,38 @@ namespace Selania.Rework.Components
     public class MainSceneController : MonoBehaviour, IAutomaticEditorInject
     {
         /// <summary>
+        ///     The logger for this controller.
+        /// </summary>
+        [Inject] internal ILogger<MainSceneController> Logger = null!;
+
+        /// <summary>
         ///     The part of the story manager that handles serialization (start new, start from savefile, etc)
         /// </summary>
         [Inject] internal IStoryStateSerializer StoryStateSerializer = null!;
 
         private void Start()
         {
-            // start the story as soon as the scene is loaded
-            StoryStateSerializer.StartStory(null);
+            StartAsync().Forget();
+        }
+
+        private async UniTaskVoid StartAsync()
+        {
+            try
+            {
+                // first check if there's at least one save: if so, load it and return
+                await foreach (var saveState in StoryStateSerializer.GetSaveStates())
+                {
+                    await StoryStateSerializer.StartStory(saveState.Descriptor);
+                    return;
+                }
+
+                // otherwise, start the story as soon as the scene is loaded
+                await StoryStateSerializer.StartStory(null);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Error while trying to load the latest save or starting the game.");
+            }
         }
     }
 }
